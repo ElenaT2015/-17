dd824e507532d7b6d9f8db33721823c04152a91e
Tsygankovae.a <Tsygankovae.a@outlook.com>
2021-12-28 01:17:38 +0300

    Добавьте файлы проекта.

---
+++ Задание 17/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой номер");
+            int numInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сумму");
+            int sum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            string name = Console.ReadLine();
+            kliyent<int> kliyent1 = new kliyent<int>(numInt, sum, name);
+            Console.WriteLine(kliyent1.GetInfo());
+
+            Console.WriteLine("Введите строковый номер");
+            string numString = Console.ReadLine();
+            Console.WriteLine("Введите ,баланс");
+            sum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            name = Console.ReadLine();
+            kliyent<string> kliyent2 = new kliyent<string>(numString, sum, name);
+            Console.WriteLine(kliyent1.GetInfo());
+
+            kliyent<int> kliyents1 = new kliyent<int> (numInt, sum, name);
+            kliyents1.Input();
+               Console.WriteLine(kliyents1.GetInfo());
+
+            kliyent<string> kliyents2 = new kliyent<string>(numString, sum, name);
+            kliyents2.Input();
+            Console.WriteLine(kliyents2.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание 17/kliyent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_17
+{
+    class kliyent<T>
+    {
+        public T Num { get; set; }
+        public int Sum { get; set; }
+        public string Name { get; set; }
+
+        public kliyent(T num, int sum, string name)
+        {
+            Num = num;
+            Sum = sum;
+            Name = name;
+        }
+
+        public string GetInfo()
+        {
+            return $"{Num} {Sum} {Name}";
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер");
+            string numString = Console.ReadLine();
+            Num = (T)Convert.ChangeType(numString, typeof(T));
+            Console.WriteLine("Введите сумму");
+            Sum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+        }
+    }
+}
